b737eff9398d5a5efff0746eb59212d8073408e0
mfauzanfathan <mhmmdfauzan19@gmail.com>
2023-11-30 23:42:05 +0700

    menambahkan validation

--- src/main/java/com/ubl/jwp/CoursesController.java
+++ src/main/java/com/ubl/jwp/CoursesController.java
@@ -4,51 +4,132 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ubl.jwp.domain.Courses;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
 public class CoursesController {
-    public static Map<String, Courses> coursesMap = new HashMap<>();
+     public static Map<String, Courses> courseMap = new HashMap<>();
 
     @GetMapping("/courses")
-    public List<Courses> getCourses() {
-        return coursesMap.values().stream().toList();
+    public String getCourses(Model model) {
+        model.addAttribute("courses", fetchCourses());
+        return "index";
     }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(Courses courses) {
+        return "add-courses";
+    }
+
     @PostMapping("/courses")
-    public ResponseEntity<String> addCourses(@RequestBody Courses courses) {
-        coursesMap.put(courses.getcoursesCode(), courses);
-        Courses savedCourses = coursesMap.get(courses.getcoursesCode());
-        return new ResponseEntity<>("Courses with courses code: " + savedCourses.getcoursesCode() +
-                "has been created", HttpStatus.OK);
+    public String addCourse(@Valid Courses course, BindingResult bindingResult, Model model) {
+
+        validateCourseCode(course.getCourseCode(), bindingResult);
+        validateTotSKS(course.getTotSKS(), bindingResult);
+        validateFaculty(course.getFaculty(), bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "add-courses";
+        }
+
+        if (isCourseCodeAlreadyExists(course.getCourseCode())) {
+            throw new IllegalArgumentException("Course with courseCode:" + course.getCourseCode() + " already exists");
+        }
+
+        courseMap.put(course.getCourseCode(), course);
+        model.addAttribute("courses", fetchCourses());
+        return "index";
     }
-    @GetMapping(value = "/courses/{coursesCode}")
-    public ResponseEntity<Courses> findCourses(@PathVariable("codeCourses") String coursesCode) {
-        final Courses courses = coursesMap.get(coursesCode);
+
+    private void validateCourseCode(String courseCode, BindingResult bindingResult) {
+        if (courseCode == null || !courseCode.matches("^PG\\d{3}$")) {
+            ObjectError error = new ObjectError("courseCode", "Code Course must start with PG and end with 3 digits");
+            bindingResult.addError(error);
+        }
+    }
+
+    private void validateTotSKS(Integer totSKS, BindingResult bindingResult) {
+        if (totSKS == null || totSKS < 1 || totSKS > 3) {
+            ObjectError error = new ObjectError("totSKS", "Total SKS must be between 1 and 3");
+            bindingResult.addError(error);
+        }
+    }
+
+    private void validateFaculty(String faculty, BindingResult bindingResult) {
+        if (faculty == null || !(faculty.equals("FE") || faculty.equals("FTI") || faculty.equals("FEB") ||
+                faculty.equals("FT") || faculty.equals("FISSIP") || faculty.equals("FKDK"))) {
+            ObjectError error = new ObjectError("faculty", "Invalid faculty. Choose from: FE, FTI, FEB, FT, FISSIP, FKDK");
+            bindingResult.addError(error);
+        }
+    }
+
+    private boolean isCourseCodeAlreadyExists(String courseCode) {
+        return courseMap.containsKey(courseCode);
+    }
+
+	@GetMapping(value = "/courses/{courseCode}")
+    public ResponseEntity<Courses> findCourses(@PathVariable("courseCode") String courseCode) {
+        final Courses courses = courseMap.get(courseCode);
         return new ResponseEntity<>(courses, HttpStatus.OK);
     }
-    @PutMapping(value = "/courses/{coursesCode}")
-    public ResponseEntity<String> updateCourses(@PathVariable("coursesCode") String coursesCode,
-            @RequestBody Courses courses) {
-        final Courses coursesToBeUpdated = coursesMap.get(courses.getcoursesCode());
-        coursesToBeUpdated.setcoursesName(courses.getcoursesName());
-        coursesToBeUpdated.setfaculty(courses.getfaculty());
-        coursesToBeUpdated.setsumSKS(courses.getsumSKS());
 
-        coursesMap.put(courses.getcoursesCode(), coursesToBeUpdated);
-        return new ResponseEntity<>("Courses with Code: " + coursesToBeUpdated.getcoursesCode() +
-                "has been updated", HttpStatus.OK);
+	private static List<Courses> fetchCourses() {
+        return courseMap.values().stream().toList();
     }
-    @DeleteMapping(value = "/courses/{coursesCode}")
-    public ResponseEntity<Void> deleteCourses(@PathVariable("coursesCode") String coursesCode) {
-        coursesMap.remove(coursesCode);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+
+    @PostMapping(value = "/courses/{courseCode}")
+    public String updateCourse(@PathVariable("courseCode") String courseCode,
+    @Valid Courses course,
+    BindingResult result, Model model) {
+
+        validateTotSKS(course.getTotSKS(), result);
+        validateFaculty(course.getFaculty(), result);
+
+        final Courses courseToBeUpdated = courseMap.get(courseCode);
+        if (courseToBeUpdated == null) {
+        throw new IllegalArgumentException("Course with courseCode:" + courseCode + " not found");
+        }
+
+        if (!courseCode.equals(course.getCourseCode()) && isCourseCodeAlreadyExists(course.getCourseCode())) {
+        throw new IllegalArgumentException("Course with courseCode:" + course.getCourseCode() + " already exists");
+        }
+
+        courseMap.put(course.getCourseCode(), course);
+
+        model.addAttribute("courses", fetchCourses());
+        return "redirect:/courses";
+    }
+
+
+	@GetMapping("/edit/{courseCode}")
+	public String showUpdateForm(@PathVariable("courseCode") String courseCode, Model model) {
+		final Courses coursesToBeUpdate = courseMap.get(courseCode);
+		if (coursesToBeUpdate == null) {
+			throw new IllegalArgumentException("Course with code : " + courseCode + "is not found");
+
+		}
+		model.addAttribute("courses", coursesToBeUpdate);
+		return "update-courses";
+	}
+
+	@GetMapping(value = "/courses/{courseCode}/delete")
+    public String deleteStudent(@PathVariable("courseCode") String courseCode) {
+        courseMap.remove(courseCode);
+        return "redirect:/courses";
     }
 }
