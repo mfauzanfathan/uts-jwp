e2c0fa7b7699a7e419a05d0a1c5d5ccb8ae7be30
mfauzanfathan <mhmmdfauzan19@gmail.com>
2023-11-30 23:44:28 +0700

    menambahkan validation

--- src/main/java/com/ubl/jwp/domain/Courses.java
+++ src/main/java/com/ubl/jwp/domain/Courses.java
@@ -1,36 +1,64 @@
 package com.ubl.jwp.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Courses {
-    private String coursesCode;
-    private String coursesName;
-    private String sumSKS;
+     @NotBlank(message = "course name is required")
+    @Pattern(regexp = "^PG.*", message = ("Course code must begin with 'PG'"))
+    private String courseCode;
+
+    @NotBlank(message = "course code is required")
+    @Size(min = 5, max = 10)
+    private String courseName;
+
+    @NotNull(message = "TotSKS is required")
+    @Min(value = 1, message = "Minimal 1 SKS")
+    @Max(value = 3, message = "Maximal 3 SKS")
+    private Integer totSKS;
+
+    @NotBlank(message = "faculty is required")
+    @Pattern(regexp = "^(FE|FTI|FT)$", message = ("Faculty must be one of (FE, FTI, FT)"))
     private String faculty;
 
-    public Courses(){
+    public Courses() {
 
     }
-    public String getcoursesCode(){
-        return coursesCode;
+
+    public String getCourseCode() {
+        return courseCode;
     }
-    public void setcourseCode(String coursesCode){
-        this.coursesCode = coursesCode;
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
-    public String getcoursesName(){
-        return coursesName;
+
+    public String getCourseName() {
+        return courseName;
     }
-    public void setcoursesName(String coursesName){
-        this.coursesName = coursesName;
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
     }
-    public String getsumSKS(){
-        return sumSKS;
-    }
-    public void setsumSKS(String sumSKS){
-        this.sumSKS = sumSKS;
-    }
-    public String getfaculty(){
+
+
+    public String getFaculty() {
         return faculty;
     }
-    public void setfaculty(String faculty){
+
+    public void setFaculty(String faculty) {
         this.faculty = faculty;
+    }
+
+    public Integer getTotSKS() {
+        return totSKS;
+    }
+
+    public void setTotSKS(Integer totSKS) {
+        this.totSKS = totSKS;
     }
 }
